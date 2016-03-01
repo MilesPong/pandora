70275b629e1cf30b1ab43e5573d8b74f31222c0b
miles@Oscar <mingpeng16@gmail.com>
2016-03-01 17:38:03 +0800

    format frontend

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -11,41 +11,41 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
-	'modules' => [
-			'user' => [
-					// following line will restrict access to admin page
-					'as frontend' => 'dektrium\user\filters\FrontendFilter'
-			]
-	],
+    'modules' => [
+            'user' => [
+                    // following line will restrict access to admin page
+                    'as frontend' => 'dektrium\user\filters\FrontendFilter'
+            ]
+    ],
     'components' => [
         'user' => [
 //             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-			'identityCookie' => [ 
-					'name' => '_frontendIdentity',
-					'path' => '/',
-					'httpOnly' => true 
-			],
+            'identityCookie' => [ 
+                    'name' => '_frontendIdentity',
+                    'path' => '/',
+                    'httpOnly' => true 
+            ],
         ],
-// 		'urlManager' => [
-// 				'enablePrettyUrl' => true,
-// 				'showScriptName' => false,
-// 				'class' => 'yii\web\UrlManager',
-// 		],
-		'urlManagerBackend' => [
-				// here is your Back-end URL rules
-				'class' => 'yii\web\urlManager',
-				'baseUrl' => 'Your Backend URL', // Your Backend URL,e.g: '//backend.dev'
-				'enablePrettyUrl' => true,
-				'showScriptName' => false 
-		],
-		'session' => [ 
-				'name' => 'FRONTENDSESSID',
-				'cookieParams' => [ 
-						'httpOnly' => true,
-						'path' => '/' 
-				] 
-		],
+//         'urlManager' => [
+//                 'enablePrettyUrl' => true,
+//                 'showScriptName' => false,
+//                 'class' => 'yii\web\UrlManager',
+//         ],
+        'urlManagerBackend' => [
+                // here is your Back-end URL rules
+                'class' => 'yii\web\urlManager',
+                'baseUrl' => 'Your Backend URL', // Your Backend URL,e.g: '//backend.dev'
+                'enablePrettyUrl' => true,
+                'showScriptName' => false 
+        ],
+        'session' => [ 
+                'name' => 'FRONTENDSESSID',
+                'cookieParams' => [ 
+                        'httpOnly' => true,
+                        'path' => '/' 
+                ] 
+        ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
